45eb1ca8014791b0957c638510f80d92078c156e
Novatouch <slurp.miammiam@gmail.com>
2012-05-05 18:04:33 +0200

    correction erreur balise pour le css

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
 
 	<title></title>
 
-	<link rel="stylesheet" media="screen" href="ecran.cs
+	 <link rel="stylesheet" media="screen" href="css/ecran.css">
 </head>
 <body>
 
 <!-- Document -->
 <?php
 	//inclusion du header
+	
 	//inclusion du menu user_tab
 
 	// récupération du paramètre page et inclusion de la bonne interface
