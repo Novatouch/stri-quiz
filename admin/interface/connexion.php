63e86db2c530f6a4393eda72a456f7bf180cd23e
Novatouch <slurp.miammiam@gmail.com>
2012-05-05 18:30:50 +0200

    Ajout d'un formulaire

---
+++ admin/interface/connexion.php
@@ -0,0 +1,12 @@
+<div id="interface_connexion-formulaire">
+	<form>
+		<p>
+			<label for="identifiant">Idantifiant :</label>
+			<input type="text" name="identifiant" id="identifiant" />
+			<label for="mdp">Mot de passe :</label>
+			<input type="text" name="mdp" id="mdp" />
+			<input type="submit" name="connexion" id="connexion" />
+		</p>
+			
+	</form>
+<div>
