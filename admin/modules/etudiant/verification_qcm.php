6626114c05fc26aac24574d9456ced73530489ca
Novatouch <slurp.miammiam@gmail.com>
2012-05-09 20:17:14 +0200

    module pour la verification du qcm

---
+++ admin/modules/etudiant/verification_qcm.php
@@ -0,0 +1,15 @@
+<?php
+
+// Ce module récupère la liste des questions et des propositions
+
+// vérification que l'utilisateur est authentifié
+
+// Récupération du paramètre $_POST['id_qcm']
+
+// vérifie que l'utilisateur à bien le droit d'accéder au QCM
+
+// récupère toutes les questions et les propositions du QCM
+
+// fabrication de l'objet JSON
+
+?>
