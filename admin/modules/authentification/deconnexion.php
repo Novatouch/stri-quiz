4897a546af6a5d094a992ae23860749a9b4f276a
Philippe GAUTIER <slurp.miammiam@gmail.com>
2012-05-13 19:04:04 +0200

    Ajout module deconnexion

---
+++ admin/modules/authentification/deconnexion.php
@@ -0,0 +1,8 @@
+<?php
+
+// Ce module supprime les sessions de l'utilisateur et le redirige vers la page de connexion
+
+// supprssion des varaibles de sessions unset()
+
+// redirection page de connexion
+?>
