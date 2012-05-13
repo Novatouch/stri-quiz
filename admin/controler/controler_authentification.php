65a6d1851e7c61cfd6090825bac39edab83fa8e9
Philippe GAUTIER <slurp.miammiam@gmail.com>
2012-05-13 18:58:42 +0200

    Ajout d'un controleur pour l'authentification

---
+++ admin/controler/controler_authentification.php
@@ -0,0 +1,33 @@
+
+<?php
+
+	// securisation des variables GET et POST
+
+	// récupération du nom du module stocké dans $_POST['module']
+	if (isset($_POST['module'])
+	{
+		$module = $_POST['module'];
+	}
+	else
+	{
+		$module = "MisterAoun";
+	}
+
+	// inclusion du module 
+
+	switch ($module) 
+	{
+		case "connexion" :
+			include_once("../modules/authentification/connexion.php");
+
+		break;
+		case "deconnexion" :
+			include_once("modules/authentification/deconnexion.php");
+
+		break;
+		default :
+			echo "big problem";
+		break;
+	}
+
+?>
