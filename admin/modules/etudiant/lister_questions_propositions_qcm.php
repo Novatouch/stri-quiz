456d13f4e68558132292bf426e7f30c03aa11754
Novatouch <slurp.miammiam@gmail.com>
2012-05-16 22:52:40 +0200

    JSON précision sur l'organisation des données

--- admin/modules/etudiant/lister_questions_propositions_qcm.php
+++ admin/modules/etudiant/lister_questions_propositions_qcm.php
@@ -11,5 +11,17 @@
 // récupère toutes les questions et les propositions du QCM
 
 // fabrication de l'objet JSON
+/* format:
+$qcm[0]["id"]= 1;
+$qcm[0]["intitule"]= "Q'est ce qu'un scanneur";
+$qcm[0]["propositions"][0]["id"]= 1;
+$qcm[0]["propositions"][0]["intitule"]= "Nmap";
+$qcm[0]["propositions"][1]["id"]= 2;
+$qcm[0]["propositions"][1]["intitule"]= "telnet";
+$qcm[0]["propositions"][2]["id"]= 3;
+$qcm[0]["propositions"][2]["intitule"]= "Nessus" ;
+$qcm[0]["propositions"][3]["id"]= 4;
+$qcm[0]["propositions"][3]["intitule"]=  "OpenVas" ;
+*/
 
 ?>
