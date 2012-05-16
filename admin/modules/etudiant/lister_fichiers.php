6d318696fbddc49f6d9dbc58a2fd4e6993baa953
Novatouch <slurp.miammiam@gmail.com>
2012-05-16 22:54:42 +0200

    Ajout module lister fichiers

---
+++ admin/modules/etudiant/lister_fichiers.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ce modules récupère la liste des fichiers corespondant à un cours
+
+
+
+// récupération de l'id du cours passée par un paramètre de type POST nom variable $_POST['id_cours']
+
+
+// requête à la base de donnée pour récupérer l'id des fichiers et les noms des fichiers
+
+
+// traitement des résultats et fabrication de l'objet JSON
+/*
+ $fichiers[0]['id'] = 1;
+ $fichiers[0]['nom'] = "NomFichiers";
+*/
+echo json_encode($fichiers);
+?>
