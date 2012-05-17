647b0afd7687ecca6edd31e7abae10ea5f2bc369
Novatouch <slurp.miammiam@gmail.com>
2012-05-17 16:51:35 +0200

    JSON ajout de précision au module

--- admin/modules/etudiant/verification_qcm.php
+++ admin/modules/etudiant/verification_qcm.php
@@ -4,12 +4,43 @@
 
 // vérification que l'utilisateur est authentifié
 
-// Récupération du paramètre $_POST['id_qcm']
+// Récupération du paramètre $_POST, organisation de l'array
+/*
+	$_POST['id_qcm'] id qcm
+	$_POST['question']
+	$_POST['question'][0]['id']
+	$_POST['question'][0]['proposition']
+	$_POST['question'][0]['proposition'][0]['id']
+	$_POST['question'][0]['proposition'][1]['id']
 
-// vérifie que l'utilisateur à bien le droit d'accéder au QCM
+	$_POST['question'][1]['id']   id question
+	$_POST['question'][1]['proposition']
+	$_POST['question'][1]['proposition'][0]['id'] id proposition
+	$_POST['question'][1]['proposition'][1]['id']
+*/
 
-// récupère toutes les questions et les propositions du QCM
 
-// fabrication de l'objet JSON
+// vérifie que l'utilisateur à bien le droit d'accéder au QCM et recupère typeQCM
+
+	// Si le type de compte est un qcm
+	// enregistrement de la participation
+
+
+// parcours des questions dans l'array $_POST
+
+	// pour chaque question récupération des id des bonne réponses
+
+		// Si le typede qcm est examen enregistrement des propositions validés par l'utilsateur
+	
+	// mise à jour du compteur de points
+
+// si le type de qcm est examen enregistrement de la note dans la bd 
+
+// fabrication de l'objet JSON pour renvoyer la note
+//renvoi de la note
+
+$resultat['note']= ; 
+
+echo json_encode($resultat);
 
 ?>
