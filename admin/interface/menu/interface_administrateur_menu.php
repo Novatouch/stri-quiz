15dc2f61163e60a36b1a07fca80593c76f4e375a
Novatouch <slurp.miammiam@gmail.com>
2012-05-20 11:38:55 +0200

    Modifiation des interfaces utilisateurs

---
+++ admin/interface/menu/interface_administrateur_menu.php
@@ -0,0 +1,10 @@
+<!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+<li class="selected"><a href="administrateur_gestion_utilisateur">Gestion des Utilisateurs</a></li>
+<li> <a href="administrateur_gestion_matiere">Gestion des matières</a></li>
+<li> <a href="administrateur_gestion_groupe">Gestion des groupes</a></li>
+<li> <a href="administrateur_gestion_formations">Gestion des formations</a></li>
+<?php
+	include_once("interface_enseignant_menu.php");
+?>
+
+
---
+++ admin/interface/menu/interface_enseignant_menu.php
@@ -0,0 +1,4 @@
+<!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+<li class="selected"><a href="enseignant_gestion_cours">Gestion des cours</a></li>
+<li><a href="enseignant_gestion_qcm">Gestion des Quiz</a></li>
+<li><a href="enseignant_gestion_fichiers">Gestions des fichiers</a></li>
