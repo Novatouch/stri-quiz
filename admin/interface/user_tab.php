5e44ca5b163e50c320994496d216bd7c82720276
Novatouch <slurp.miammiam@gmail.com>
2012-05-20 11:33:56 +0200

    Modification du design

--- admin/interface/user_tab.php
+++ admin/interface/user_tab.php
@@ -1,7 +1,29 @@
-<div id="user_tab">
+
 <?php
 	// vérifie les variables de sessions avant d'inclure le contenu
 	
 	// Affiche Nom,Prenom, role 
 ?>
-</div>
+<!-- insert your sidebar items here -->
+        <h3>Latest News</h3>
+        <h4>New Website Launched</h4>
+        <h5>January 1st, 2011</h5>
+        <p>2011 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+        <p></p>
+        <h4>New Website Launched</h4>
+        <h5>January 1st, 2011</h5>
+        <p>2011 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+        <h3>Useful Links</h3>
+        <ul>
+          <li><a href="#">link 1</a></li>
+          <li><a href="#">link 2</a></li>
+          <li><a href="#">link 3</a></li>
+          <li><a href="#">link 4</a></li>
+        </ul>
+        <h3>Search</h3>
+        <form method="post" action="#" id="search_form">
+          <p>
+            <input class="search" type="text" name="search_field" value="Enter keywords....." />
+            <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+          </p>
+        </form>
