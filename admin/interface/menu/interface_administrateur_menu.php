e463fa88522c88f73cad0ffdb5f79cd2ea675647
Novatouch <slurp.miammiam@gmail.com>
2012-05-21 13:18:13 +0200

    Modification du menu administrateur

--- admin/interface/menu/interface_administrateur_menu.php
+++ admin/interface/menu/interface_administrateur_menu.php
@@ -3,8 +3,7 @@
 <li> <a href="administrateur_gestion_matiere">Gestion des matières</a></li>
 <li> <a href="administrateur_gestion_groupe">Gestion des groupes</a></li>
 <li> <a href="administrateur_gestion_formations">Gestion des formations</a></li>
-<?php
-	include_once("interface_enseignant_menu.php");
-?>
-
+<li> <a href="enseignant_gestion_cours">Gestion des cours</a></li>
+<li> <a href="enseignant_gestion_qcm">Gestion des Quiz</a></li>
+<li> <a href="enseignant_gestion_fichiers">Gestions des fichiers</a></li>
 
