db0b3488dc69b101b0431b39c4d461ccff69fed1
Novatouch <slurp.miammiam@gmail.com>
2012-05-27 13:16:48 +0200

    Correction d'erreurs + changement parametre POST -> GET

--- admin/controler/controler_etudiant.php
+++ admin/controler/controler_etudiant.php
@@ -6,9 +6,9 @@
 	// securisation des variables GET et POST
 
 	// récupération du nom du module stocké dans $_POST['module']
-	if (isset($_POST['module'])
+	if (isset($_GET['module']))
 	{
-		$module = $_POST['module'];
+		$module = $_GET['module'];
 	}
 	else
 	{
@@ -19,35 +19,35 @@
 
 	switch ($module) 
 	{
-		case "lister_matieres" :
-			include_once("../modules/etudiant/lister_matiere.php");
+		case "lister_matieres":
+			include_once("../modules/etudiant/lister_matieres.php");
 
 		break;
-		case "lister_cours" :
-			include_once("modules/etudiant/lister_cours.php");
+		case "lister_cours":
+			include_once("../modules/etudiant/lister_cours.php");
 
 		break;
-		case "lister_QCM" :
-			include_once("modules/etudiant/lister_QCM.php");
+		case "lister_QCM":
+			include_once("../modules/etudiant/lister_QCM.php");
 
 		break;
-		case "lister_fichiers" :
+		case "lister_fichiers":
 			include_once("modules/etudiant/lister_fichiers.php");
 
 		break;
-		case "lister_questions_propositions_qcm" :
+		case "lister_questions_propositions_qcm":
 			include_once("modules/etudiant/lister_questions_propositions_qcm.php");
 
 		break;
-		case "telechargement_fichier" :
+		case "telechargement_fichier":
 			include_once("modules/etudiant/telechargement_fichier.php");
 
 		break;
-		case "verification_qcm" :
+		case "verification_qcm":
 			include_once("modules/etudiant/verification_qcm.php");
 
 		break;
-		default :
+		default:
 			echo "big problem";
 		break;
 	}
