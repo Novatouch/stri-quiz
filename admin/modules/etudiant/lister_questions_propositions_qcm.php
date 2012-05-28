8240c7c5371c6a201b53e0d0ecc9ded481e342a1
Novatouch <slurp.miammiam@gmail.com>
2012-05-28 20:46:49 +0200

    Mise à jour de variables et declaration de variable pour test

--- admin/modules/etudiant/lister_questions_propositions_qcm.php
+++ admin/modules/etudiant/lister_questions_propositions_qcm.php
@@ -4,16 +4,16 @@
 
 // vérification que l'utilisateur est authentifié
 
-// Récupération du paramètre $_POST['id_qcm']
+// Récupération du paramètre $_GET['idqcm']
 
 // vérifie que l'utilisateur à bien le droit d'accéder au QCM
 
 // récupère toutes les questions et les propositions du QCM
 
 // fabrication de l'objet JSON
-/* format:
-$qcm[0]["id"]= 1;
-$qcm[0]["intitule"]= "Q'est ce qu'un scanneur";
+
+$qcm[0]["id"]= "gnagna";
+$qcm[0]["intitule"]= "Qu'est ce que la foudre";
 $qcm[0]["propositions"][0]["id"]= 1;
 $qcm[0]["propositions"][0]["intitule"]= "Nmap";
 $qcm[0]["propositions"][1]["id"]= 2;
@@ -22,6 +22,26 @@ $qcm[0]["propositions"][2]["id"]= 3;
 $qcm[0]["propositions"][2]["intitule"]= "Nessus" ;
 $qcm[0]["propositions"][3]["id"]= 4;
 $qcm[0]["propositions"][3]["intitule"]=  "OpenVas" ;
-*/
+$qcm[1]["id"]= 2;
+$qcm[1]["intitule"]= "Qu'est ce que la sécurité";
+$qcm[1]["propositions"][0]["id"]= 5;
+$qcm[1]["propositions"][0]["intitule"]= "Nmap";
+$qcm[1]["propositions"][1]["id"]= 6;
+$qcm[1]["propositions"][1]["intitule"]= "telnet";
+$qcm[1]["propositions"][2]["id"]= 7;
+$qcm[1]["propositions"][2]["intitule"]= "Nessus" ;
+$qcm[1]["propositions"][3]["id"]= 8;
+$qcm[1]["propositions"][3]["intitule"]=  "OpenVas" ;
+$qcm[2]["id"]= 3;
+$qcm[2]["intitule"]= "Q'est ce qu'un scanneur";
+$qcm[2]["propositions"][0]["id"]= 9;
+$qcm[2]["propositions"][0]["intitule"]= "Nmap";
+$qcm[2]["propositions"][1]["id"]= 10;
+$qcm[2]["propositions"][1]["intitule"]= "telnet";
+$qcm[2]["propositions"][2]["id"]= 11;
+$qcm[2]["propositions"][2]["intitule"]= "Nessus" ;
+$qcm[2]["propositions"][3]["id"]= 12;
+$qcm[2]["propositions"][3]["intitule"]=  "OpenVas" ;
+echo json_encode($qcm);
 
 ?>
--- admin/modules/etudiant/verification_qcm.php
+++ admin/modules/etudiant/verification_qcm.php
@@ -4,31 +4,36 @@
 
 // vérification que l'utilisateur est authentifié
 
-// Récupération du paramètre $_POST, organisation de l'array
+// Récupération du paramètre $_GET, organisation de l'array
 /*
-	$_POST['id_qcm'] id qcm
-	$_POST['question']
-	$_POST['question'][0]['id']
-	$_POST['question'][0]['proposition']
-	$_POST['question'][0]['proposition'][0]['id']
-	$_POST['question'][0]['proposition'][1]['id']
+	$_GET['id_qcm'] id qcm
+	$_GET['question']
+	$_GET['question'][0]['id']
+	$_GET['question'][0]['proposition']
+	$_GET['question'][0]['proposition'][0]['id']
+	$_GET['question'][0]['proposition'][1]['id']
 
-	$_POST['question'][1]['id']   id question
-	$_POST['question'][1]['proposition']
-	$_POST['question'][1]['proposition'][0]['id'] id proposition
-	$_POST['question'][1]['proposition'][1]['id']
+	$_GET['question'][1]['id']   id question
+	$_GET['question'][1]['proposition']
+	$_GET['question'][1]['proposition'][0]['id'] id proposition
+	$_GET['question'][1]['proposition'][1]['id']
 */
 
 
 // vérifie que l'utilisateur à bien le droit d'accéder au QCM et recupère typeQCM
 
-	// Si le type de compte est un qcm
+	// Si le type de compte est un qcm d'évaluation
 	// enregistrement de la participation
 
 
-// parcours des questions dans l'array $_POST
+// parcours des questions dans l'array $_GET
 
-	// pour chaque question récupération des id des bonne réponses
+	// pour chaque question 
+	//incrémentation du compteur de question
+
+
+	//vérifier si l'utilisateur a coché aucune réponse isset($_GET['question'][x]['proposition']))
+ 	//récupération des id des bonne réponses
 
 		// Si le typede qcm est examen enregistrement des propositions validés par l'utilsateur
 	
@@ -39,7 +44,7 @@
 // fabrication de l'objet JSON pour renvoyer la note
 //renvoi de la note
 
-$resultat['note']= ; 
+$resultat['note']= "20"; 
 
 echo json_encode($resultat);
 
