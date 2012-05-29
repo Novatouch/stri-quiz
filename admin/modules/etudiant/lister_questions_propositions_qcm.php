794a9139c77a905b1d82aad329cbf8a194dcd305
rodul <ben258@hotmail.fr>
2012-05-29 20:19:11 +0300

    Update admin/modules/etudiant/lister_questions_propositions_qcm.php

--- admin/modules/etudiant/lister_questions_propositions_qcm.php
+++ admin/modules/etudiant/lister_questions_propositions_qcm.php
@@ -22,8 +22,8 @@ echo 'connexion réussi.';
 } else {
 echo 'connexion échoué.';
 }
-$req = "SELECT idQuiz,nomQ, FROM MATIERES,ACCEDER,UTILISATEURS,FORMATIONS,GROUPE WHERE MATIERES.idMatiere=ACCEDER.idMatiere 
-AND ACCEDER.idFormation=FORMATIONS.idFormation AND GROUPES.idFormation=FORMATIONS.idFormation AND GROUPES.idGroupe=AppartenirGroupe.idGroupe AND UTILISATEURS.idUtilisateurs=UTILISATEURS.idUtilisateurs AND idMatiere=$id_M ";
+$req = "SELECT idQuiz,intituleQ,intituleProp FROM QUIZ,QUESTIONS,PROPOSITIONS WHERE ContenirQuestion.idQuiz=QUIZ.idQuiz 
+AND ContenirQuestion.idQuestion=QUESTIONS.idQuestion AND idQuiz=$idqcm";
 $result = pg_exec($db_handle, $req);
 
 $nbcolonne=pg_numrows($result);
@@ -31,11 +31,20 @@ if ($result) {
 echo "La requête s'est bien executer.<br>\n";
 for ($row = 0; $row < $nbcolonne; $row++) {
 $values = pg_fetch_object($result, $row, PGSQL_ASSOC);
-$idm = $values->idMatiere . " ";
-$nomm .= $values->nomM . " ";
-$matieres[$row]['id'] = $idm;
- $matieres[$row]['nom'] = $nomm ;
-
+$idqz = $values->idQuiz . " ";
+$intQuestion .= $values->intituleQ . " ";
+$intProp = $values->intituleProp . " ";
+$qcm[$row][$idqz ]= $idqz ;
+$qcm[$row][$intQuestion]= $intQuestion;
+$qcm[$row][intituleProp][$row][$idqz ]=$idqz ;
+$qcm[$row][intituleProp][$row][$intQuestion]= $intQuestion;
+$qcm[$row][$intProp][$row+1][$idqz ]= $idqz ;
+$qcm[$row][$intProp][$row+2][$intQuestion]= $intQuestion;
+$qcm[$row][$intProp][$row+2][$idqz ]= $idqz ;
+$qcm[$row][$intProp][$row+2][$intQuestion]= $intQuestion ;
+$qcm[$row][$intProp][$row+3][$idqz ]=$idqz;
+$qcm[$row][$intProp][$row+3][$intQuestion]= $intQuestion;
+/*bon ici, j'ai quand meme penser que c'est assez lourd comme construction et que l'on devrait discuter sur le fait que ca serait bien d'imposer un nombre propositions minimum par question*/ 
  }
 else {
 echo "La requête à rencontrer une erreur:<br>\n";
@@ -46,7 +55,7 @@ pg_freeresult($result);
 pg_close($db_handle);
 
 // fabrication de l'objet JSON
-
+/*
 $qcm[0]["id"]= "gnagna";
 $qcm[0]["intitule"]= "Qu'est ce que la foudre";
 $qcm[0]["propositions"][0]["id"]= 1;
@@ -76,7 +85,7 @@ $qcm[2]["propositions"][1]["intitule"]= "telnet";
 $qcm[2]["propositions"][2]["id"]= 11;
 $qcm[2]["propositions"][2]["intitule"]= "Nessus" ;
 $qcm[2]["propositions"][3]["id"]= 12;
-$qcm[2]["propositions"][3]["intitule"]=  "OpenVas" ;
+$qcm[2]["propositions"][3]["intitule"]=  "OpenVas" ;*/
 echo json_encode($qcm);
 
 ?>
