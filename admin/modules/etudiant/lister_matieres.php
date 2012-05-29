2b04186f65a0a10bc5a83ca062a35f2cd8470983
rodul <ben258@hotmail.fr>
2012-05-29 19:47:22 +0300

    Update admin/modules/etudiant/lister_matieres.php

--- admin/modules/etudiant/lister_matieres.php
+++ admin/modules/etudiant/lister_matieres.php
@@ -4,9 +4,14 @@
 
 // récupération variable de type POST $_POST["id_matière"] 
 
+$id_M= $_POST["id_matière"] ;
+
 // récupération des variables de sessions contenant l'id de l'utilisateur
 
+
 // requête à la base de donnée 
+
+
 
 // traitement des résultats et fabrication de l'objet JSON
 
