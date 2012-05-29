80d1e242d10191507da1a0af0f0708814fafd124
rodul <ben258@hotmail.fr>
2012-05-29 21:23:40 +0300

    Update admin/modules/etudiant/verification_qcm.php

--- admin/modules/etudiant/verification_qcm.php
+++ admin/modules/etudiant/verification_qcm.php
@@ -8,25 +8,26 @@
 
 require(" telechargement_fichier.php");
 /*récuperation de la liste des questions et propositions du quiz*/
-$qcm
+$utils=$_POST['id_Utilisateur'];
 $reqQ="SELECT idQuiz FROM QUIZ";
 
 $idQ_etudiant=$_POST['id_quiz'];
 
-int j=pg_numrows($result);
+int j=pg_numrows($reqQ);
+
 for(int i=0;i<j;i++)
 {
 /*Ici on récupere les réponses de l'étudiant*/
-$qcm_etudiant[i][$idQ_etudiant]= $_GET['id_qcm']; 
-$qcm_etudiant[i][$int_Question]= $_GET['question'];
-$qcm_etudiant[i][intitule_Prop][i][$idQ_etudiant]=$_GET['question'][i]['id'];
-$qcm_etudiant[i][intitule_Prop][i][$int_Question]= $_GET['question'][i]['proposition'];
-$qcm_etudiant[i][intitule_Prop][i+1][$int_Question]=$_GET['question'][i]['proposition'][i]['id'];
-$qcm_etudiant[i][intitule_Prop][i+2][$int_Question]= $_GET['question'][i]['proposition'][i]['id'];
-$qcm_etudiant[i][intitule_Prop][i+2][$int_Question]= $_GET['question'][i]['id'] ; ;
-$qcm_etudiant[i][intitule_Prop][i+2][$int_Question]= $_GET['question'][i]['proposition']; 
-$qcm_etudiant[i][intitule_Prop][i+3][$int_Question]=$_GET['question'][i]['proposition'][i]['id'] ;
-$qcm_etudiant[i][intitule_Prop][i+3][$int_Question]= $_GET['question'][i]['proposition'][i]['id'];
+$qcm_etudiant[$idQ_etudiant][$idQ_etudiant]= $_GET['id_qcm']; 
+$qcm_etudiant[$idQ_etudiant][$int_Question]= $_GET['question'];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant][$idQ_etudiant]=$_GET['question'][$idQ_etudiant][$idQ_etudiant];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant][$int_Question]= $_GET['question'][$idQ_etudiant]['proposition'];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+1][$int_Question]=$_GET['question'][$idQ_etudiant]['proposition'][i]['id'];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][$int_Question]= $_GET['question'][$idQ_etudiant]['proposition'][i]['id'];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][$int_Question]= $_GET['question'][$idQ_etudiant][$idQ_etudiant] ;
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][$int_Question]= $_GET['question'][$idQ_etudiant]['proposition']; 
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+3][$int_Question]=$_GET['question'][$idQ_etudiant]['proposition'][i]['id'] ;
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+3][$int_Question]= $_GET['question'][$idQ_etudiant]['proposition'][i]['id'];
 
 }
 	/*$_GET['id_qcm'];
@@ -45,18 +46,39 @@ $qcm_etudiant[i][intitule_Prop][i+3][$int_Question]= $_GET['question'][i]['propo
 
 // vérifie que l'utilisateur à bien le droit d'accéder au QCM et recupère typeQCM
 
-$req = "SELECT typeQuizz FROM QUIZZ WHERE idQuiz= $idQ_etudiant";
 
-	// Si le type de compte est un qcm d'évaluation
-	// enregistrement de la participation
-
-if(
 // parcours des questions dans l'array $_GET
 
 	// pour chaque question 
 	//incrémentation du compteur de question
-
-
+i=0;
+for(int p=0;i<j;i++)
+{
+if( $qcm_etudiant[$idQ_etudiant][$idQ_etudiant]= $qcm[$idQ_etudiant][$idQ_etudiant]; 
+$qcm_etudiant[$idQ_etudiant][p]= $qcm[i][$int_Question];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant][$idQ_etudiant]=$qcm[$idQ_etudiant][intitule_Prop][$idQ_etudiant][$idQ_etudiant];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant][p]=$qcm[$idQ_etudiant][intitule_Prop][$idQ_etudiant][p];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+1][p]=$qcm[$idQ_etudiant][intitule_Prop][$idQ_etudiant+1][$int_Question];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][p]= $qcm[$idQ_etudiant][intitule_Prop][i+2][p];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][p]= $qcm[$idQ_etudiant][intitule_Prop][i+2][p] ; 
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][p]= $qcm[$idQ_etudiant][intitule_Prop][i+2][p]; 
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+3][p]=$qcm[$idQ_etudiant][intitule_Prop][i+3][p];
+$qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+3][p]= $qcm[$idQ_etudiant][intitule_Prop][i+3][p];)
+{note=note+1;}
+else if( (empty($qcm_etudiant[i][$idQ_etudiant]) && 
+(empty($qcm_etudiant[$idQ_etudiant][p])) &&
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant][$idQ_etudiant])) &&
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant][p])) &&
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+1][p])) &&
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][p])) &&
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][p])) && 
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+2][p])) && 
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+3][p])) &&
+(empty($qcm_etudiant[$idQ_etudiant][intitule_Prop][$idQ_etudiant+3][p])))
+{note=note+0;}
+else
+{note=note-1;}
+}
 
 
 	//vérifier si l'utilisateur a coché aucune réponse isset($_GET['question'][x]['proposition']))
@@ -64,6 +86,17 @@ if(
 
 		// Si le type de qcm est examen enregistrement des propositions validés par l'utilsateur
 	
+$req = "SELECT typeQuizz FROM QUIZZ WHERE idQuiz= $idQ_etudiant";
+
+	// Si le type de compte est un qcm d'évaluation
+	// enregistrement de la participation
+$values = pg_fetch_object($req , $row, PGSQL_ASSOC);
+if($values->typeQuizz=="EVALUATION"){$query_evaluation="INSERT INTO PARTICIPER(,$utils,$idQ_etudiant,,$note);pg_execute(BD_PROJET, $query_evaluation);}
+else if($values->typeQuizz=="EXAMEN"){$query_examen="INSERT INTO PARTICIPER(,$utils,$idQ_etudiant,,$note);pg_execute(BD_PROJET, $query_examen);}
+
+//A voir dans le code comment mettre la date et idParticiper correctement
+
+//Participer(idParticiper,#idUtilisateurs,#idQuiz, date,note)
 	// mise à jour du compteur de points
 
 // si le type de qcm est examen enregistrement de la note dans la bd 
@@ -71,8 +104,8 @@ if(
 // fabrication de l'objet JSON pour renvoyer la note
 //renvoi de la note
 
-$resultat['note']= "20"; 
+/*$resultat['note']= "20"; */
 
-echo json_encode($resultat);
+echo json_encode($note);
 
 ?>
