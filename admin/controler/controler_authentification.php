b59b9dff61da6bf30dd3d33f0cf13de086753e5b
Novatouch <slurp.miammiam@gmail.com>
2012-05-29 21:27:06 +0200

    Ajout module connexion

--- admin/controler/controler_authentification.php
+++ admin/controler/controler_authentification.php
@@ -1,13 +1,20 @@
 <?php
 
-	// demarrage des sessions php
-	session_start();	
+	// demarrage des sessions php et connexion bd
+	session_start();
+
+	// variable contenant le chemin du serveur
+	include_once("../../config/document_root.php");
+
+
+
+	
 	// securisation des variables GET et POST
 
 	// récupération du nom du module stocké dans $_POST['module']
-	if (isset($_POST['module'])
+	if (isset($_GET['module']))
 	{
-		$module = $_POST['module'];
+		$module = $_GET['module'];
 	}
 	else
 	{
@@ -18,17 +25,18 @@
 
 	switch ($module) 
 	{
-		case "connexion" :
+		case "connexion":
 			include_once("../modules/authentification/connexion.php");
 
 		break;
-		case "deconnexion" :
-			include_once("modules/authentification/deconnexion.php");
+		case "deconnexion":
+			include_once("../modules/authentification/deconnexion.php");
 
 		break;
-		default :
+		default:
 			echo "big problem";
 		break;
 	}
 
+	
 ?>
