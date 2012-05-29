fbb464cef364d3d2e927599757337fef8072db07
rodul <ben258@hotmail.fr>
2012-05-29 20:24:13 +0300

    Update admin/modules/etudiant/telechargement_fichier.php

--- admin/modules/etudiant/telechargement_fichier.php
+++ admin/modules/etudiant/telechargement_fichier.php
@@ -6,10 +6,18 @@
 
 // Récupération du paramètre $_POST['id_qcm']
 
+$idqcm=$_POST['id_qcm'];
+
 // vérifie que l'utilisateur à bien le droit d'accéder au QCM
+
+
 
 // récupère toutes les questions et les propositions du QCM
 
+
+
 // fabrication de l'objet JSON
+
+
 
 ?>
