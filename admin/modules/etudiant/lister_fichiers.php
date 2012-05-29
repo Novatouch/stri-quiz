ab7d3c17f07ad4b99b4878369506994916811a6d
rodul <ben258@hotmail.fr>
2012-05-29 19:46:10 +0300

    Update admin/modules/etudiant/lister_fichiers.php

--- admin/modules/etudiant/lister_fichiers.php
+++ admin/modules/etudiant/lister_fichiers.php
@@ -10,12 +10,37 @@ $id_c=$_POST['id_cours'];
 
 // requête à la base de donnée pour récupérer l'id des fichiers et les noms des fichiers
 
+$db_handle = pg_connect("BD_PROJET=$BD_PROJET");
+if ($db_handle) {
+echo 'connexion réussi.';
+} else {
+echo 'connexion échoué.';
+}
+$req = "SELECT idFichier,nomF FROM FICHIERS,COURS WHERE COURS.idCours=FICHIERS.idCours";
+$result = pg_exec($db_handle, $req);
 
+$nbcolonne=pg_numrows($result);
+if ($result) {
+echo "La requête s'est bien executer.<br>\n";
+for ($row = 0; $row < $nbcolonne; $row++) {
+$values = pg_fetch_object($result, $row, PGSQL_ASSOC);
+$idFichiers = $values->idFichier . " ";
+$nomFichiers .= $values->nomF . " ";
+$fichiers[$row]['id'] = $idFichiers;
+ $fichiers[$row]['nom'] = $nomFichiers;
+ }
+else {
+echo "La requête à rencontrer une erreur:<br>\n";
+echo pg_errormessage($db_handle);
+}
+
+pg_freeresult($result);
+pg_close($db_handle);
 
 // traitement des résultats et fabrication de l'objet JSON
 
- $fichiers[0]['id'] = 1;
- $fichiers[0]['nom'] = "NomFichiers";
+/* $fichiers[0]['id'] = 1;
+ $fichiers[0]['nom'] = "NomFichiers";*/
 
 echo json_encode($fichiers);
 ?>
