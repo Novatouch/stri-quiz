f5e4c5ddf4509b58b5006d7ce754e792b472f7f5
rodul <ben258@hotmail.fr>
2012-05-29 19:42:33 +0300

    Update admin/modules/etudiant/lister_fichiers.php

--- admin/modules/etudiant/lister_fichiers.php
+++ admin/modules/etudiant/lister_fichiers.php
@@ -6,8 +6,10 @@
 
 // récupération de l'id du cours passée par un paramètre de type POST nom variable $_GET['id_cours']
 
+$id_c=$_POST['id_cours'];
 
 // requête à la base de donnée pour récupérer l'id des fichiers et les noms des fichiers
+
 
 
 // traitement des résultats et fabrication de l'objet JSON
