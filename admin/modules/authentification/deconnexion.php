4d7d715211d371b74fec5a7d110075cdb16ea23e
Novatouch <slurp.miammiam@gmail.com>
2012-05-29 21:28:20 +0200

    module déco

--- admin/modules/authentification/deconnexion.php
+++ admin/modules/authentification/deconnexion.php
@@ -3,6 +3,17 @@
 // Ce module supprime les sessions de l'utilisateur et le redirige vers la page de connexion
 
 // supprssion des varaibles de sessions unset()
+unset($_SESSION['idUtilisateur']);
+unset($_SESSION['Nom']);
+unset($_SESSION['Prenom']);
+unset($_SESSION['pseudo']);
+unset($_SESSION['typeDeCompte']);
+
+// fin de la session
+$_SESSION = array(); // on réécrit le tableau
+session_destroy();
 
 // redirection page de connexion
+header('Location: ../../index.php');
+
 ?>
