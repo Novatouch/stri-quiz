9e0b1d8bf92e53e29205fef58c9c33a758474088
rodul <ben258@hotmail.fr>
2012-05-29 19:50:10 +0300

    Update admin/modules/etudiant/lister_cours.php

--- admin/modules/etudiant/lister_cours.php
+++ admin/modules/etudiant/lister_cours.php
@@ -4,7 +4,7 @@
 
 // récupération des variables de sessions contenant l'id de l'utilisateur
 
-// récupération de l'id de la matière passée par un paramètre de type POST nom variable $_POST['id_module']
+// récupération de l'id de la matière passée par un paramètre de type POST nom variable $_POST['idMatiere']
 $id_Matiere = $_POST['idMatiere'];
 
 // requête à la base de donnée 
@@ -16,7 +16,7 @@ echo 'connexion réussi.';
 echo 'connexion échoué.';
 }
 $req = "SELECT idCours, nomC FROM COURS,MATIERES,FORMATIONS,GROUPES,UTILISATEURS,ACCEDER,AppartenirGroupe WHERE COURS.idMatiere=MATIERES.idMatieres AND ACCEDER.idMatieres=MATIERES.idMatieres AND FORMATIONS.idFormation=ACCEDER.idFormation
-AND GROUPES.idFormation=FORMATIONS.idFormation AND AppartenirGroupe.idGroupe=GROUPES.idGroupe AND AppartenirGroupe.idUtilisateurs=UTILISATEURS.idUtilisateurs";
+AND GROUPES.idFormation=FORMATIONS.idFormation AND AppartenirGroupe.idGroupe=GROUPES.idGroupe AND AppartenirGroupe.idUtilisateurs=UTILISATEURS.idUtilisateurs AND idMatieres=$id_Matiere";
 $result = pg_exec($db_handle, $req);
 
 $nbcolonne=pg_numrows($result);
@@ -38,13 +38,13 @@ pg_freeresult($result);
 pg_close($db_handle);
 
 // traitement des résultats et fabrication de l'objet JSON
-if(!isset($_GET['idmodule']))
+if(!isset($_POST['idMatiere']))
 {
 $bool= "existepas";
 }
 else
 {
-	$bool= $_GET['idmodule'];
+	$bool= $_POST['idMatiere'];
 }
 /* $cours[0]['id'] = 1;
  $cours[0]['nom'] = $bool;
