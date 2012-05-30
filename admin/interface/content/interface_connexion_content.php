0df3846138ecb87934e639d494b75be2083091a0
Novatouch <slurp.miammiam@gmail.com>
2012-05-30 11:53:20 +0200

    modif apparence

--- admin/interface/content/interface_connexion_content.php
+++ admin/interface/content/interface_connexion_content.php
@@ -1,11 +1,11 @@
 
 <form method="post" action="admin/controler/controler_authentification.php?module=connexion">
-	<p>
-		<label for="identifiant">Idantifiant :</label>
-		<input type="text" name="identifiant" id="identifiant" />
+	<p id="log">
+		<label for="identifiant">Identifiant    :</label>
+		<input type="text" name="identifiant" id="identifiant" /></br>
 		<label for="mdp">Mot de passe :</label>
-		<input type="password" name="mdp" id="mdp" />
-		<input type="submit" name="connexion" id="connexion" />
+		<input type="password" name="mdp" id="mdp" /></br>
+		<input type="submit" name="connexion" id="connexion" /></br>
 	</p>
 		
 </form>
